d32551609b6fc3c7ab36b041b8e74a277461059e
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-06-15 23:46:03 -0300

    feat: test setup

--- src/test/groovy/com/jpdev/main/ApplicationTests.groovy
+++ src/test/groovy/com/jpdev/main/ApplicationTests.groovy
@@ -6,7 +6,7 @@ import com.amazonaws.services.dynamodbv2.model.CreateTableRequest
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
 import com.jpdev.main.domain.Product
 import com.jpdev.main.repository.ProductRepository
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -27,40 +27,24 @@ class ApplicationTests {
     ProductRepository repository;
 
     private static final String EXPECTED_COST = "20";
-    private static final String EXPECTED_PRICE = "50";
 
-//    @BeforeAll
-//    public void setup() throws Exception {
-//        dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-//
-//        CreateTableRequest tableRequest = dynamoDBMapper
-//                .generateCreateTableRequest(Product.class);
-//        tableRequest.setProvisionedThroughput(
-//                new ProvisionedThroughput(1L, 1L));
-//        amazonDynamoDB.createTable(tableRequest);
-//
-//        //...
-//
-//        dynamoDBMapper.batchDelete(
-//                (List<Product>)repository.findAll());
-//    }
+    @BeforeEach
+    public void setup() {
+        dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB)
+
+        CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(Product.class)
+        tableRequest.setProvisionedThroughput(
+                new ProvisionedThroughput(1L, 1L)
+        )
+        amazonDynamoDB.createTable(tableRequest);
+
+        dynamoDBMapper.batchDelete(
+                (List<Product>)repository.findAll()
+        )
+    }
 
     @Test
     public void givenItemWithExpectedCost_whenRunFindAll_thenItemIsFound() {
-
-        dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-
-        CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(Product.class);
-        tableRequest.setProvisionedThroughput(
-                new ProvisionedThroughput(1L, 1L)
-        );
-        amazonDynamoDB.createTable(tableRequest);
-
-        //...
-
-        dynamoDBMapper.batchDelete(
-                (List<Product>)repository.findAll());
-
         Product productInfo = new Product();
         productInfo.setCost(EXPECTED_COST);
         println(productInfo.getCost());
@@ -68,8 +52,7 @@ class ApplicationTests {
         repository.save(productInfo);
         List<Product> result = (List<Product>) repository.findAll();
 
-
-//        assertTrue(result.size(), is(greaterThan(0)));
-//        assertThat(result.get(0).getCost(), is(equalTo(EXPECTED_COST)));
+        assert (result.size() == 1)
+        assert (result.get(0).getCost() == EXPECTED_COST)
     }
 }
